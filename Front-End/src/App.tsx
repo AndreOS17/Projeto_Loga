ac809fd4097d91f22866f3c080d08883c16fed34
AndreOS17 <andre.os908@gmail.com>
2021-10-26 18:04:39 -0300

    :lipstick: Desenvolvimento de Estilo da Aplicação

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 
+import './global.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="Container">
-        <div className="Card">
-          <h1 className="Card_Title">Loga</h1>
-          <hr/>
-          <form>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email"/>
+    <div className="Container">
+      <div className="Card">
+        <h1 className="Card_Title">Loga</h1>
+        <hr />
+        <form>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
 
-            <label htmlFor="senha">Senha</label>
-            <input type="password" id="senha" />
+          <label htmlFor="senha">Senha</label>
+          <input type="password" id="senha" />
 
-            <button type="submit">Continuar</button>
-          </form>
-        </div>
+          <button type="submit">Continuar</button>
+        </form>
       </div>
     </div>
   );
