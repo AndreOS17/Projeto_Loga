bc1d7ff1df2d1fb7be33e958cc7e355eb6f4c1d0
AndreOS17 <andre.os908@gmail.com>
2021-11-03 17:12:12 -0300

    :construction: Implementação de Funcionalidade

--- Back-End/main.py
+++ Back-End/main.py
@@ -1,2 +1,28 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api, Resource, reqparse
 
-if __name__ == '__main__': pass
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app)
+
+Login_POST_args = reqparse.RequestParser()
+Login_POST_args.add_argument(
+    'email', type=str
+)
+Login_POST_args.add_argument(
+    'password', type=str
+)
+
+class Login(Resource):
+    def post(self):
+        args = Login_POST_args.parse_args()
+        if args['email'] and args['password']: 
+            pass # Adicionar ao "Banco de Dados"
+        else: 
+            pass # Devolver mensagem de erro!
+        # return args
+        
+api.add_resource(Login, '/Login')
+
+if __name__ == '__main__': app.run(debug=True)
