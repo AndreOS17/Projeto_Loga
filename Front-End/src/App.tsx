63fd407120b60ae0e56c695f283a7ebf3ebe2a0d
AndreOS17 <andre.os908@gmail.com>
2021-11-03 17:11:21 -0300

    :sparkles: Adição de API própria

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+
+import api from './api';
 
 import './global.css';
 import './App.css';
 
 function App() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function Login(e: FormEvent) {
+    e.preventDefault();
+    const response = await api.post('/Login', {email, password});
+    console.log(response.data);
+  }
+
   return (
     <div className="Container">
       <div className="Card">
         <h1 className="Card_Title">Loga</h1>
         <hr />
-        <form>
+        <form onSubmit={Login}>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" value={email} onChange={(e) => {
+            setEmail(e.target.value);
+          }}/>
 
           <label htmlFor="senha">Senha</label>
-          <input type="password" id="senha" />
+          <input type="password" id="senha" value={password} onChange={(e) => {
+            setPassword(e.target.value);
+          }}/>
 
           <button type="submit">CONTINUAR</button>
         </form>
